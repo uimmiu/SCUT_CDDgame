b3379f92b71328d1bc5b8aa14687dd527d6885e1
JHSeng <zxc98567@gmail.com>
2019-05-24 17:54:34 +0800

    format code

--- app/src/main/java/com/example/scut_cddgame/MainActivity.java
+++ app/src/main/java/com/example/scut_cddgame/MainActivity.java
@@ -17,22 +17,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //玩家点击START按钮事件
-    public void btnStartClicked(View view){
+    public void btnStartClicked(View view) {
         setContentView(R.layout.login_view);
     }
 
     //玩家点击QUIT按钮事件
-    public void btnQuitClicked(View view){
-        AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(this);
+    public void btnQuitClicked(View view) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setMessage("你确定要退出程序吗？");
-        alertDialogBuilder.setPositiveButton("确定",killProgram);
-        alertDialogBuilder.setNegativeButton("取消",cancelKillProgram);
-        AlertDialog alertDialog=alertDialogBuilder.create();
+        alertDialogBuilder.setPositiveButton("确定", killProgram);
+        alertDialogBuilder.setNegativeButton("取消", cancelKillProgram);
+        AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     }
 
     //玩家选择退出游戏时，杀死进程
-    private DialogInterface.OnClickListener killProgram=new DialogInterface.OnClickListener() {
+    private DialogInterface.OnClickListener killProgram = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
             android.os.Process.killProcess(android.os.Process.myPid());
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     //取消退出游戏
-    private DialogInterface.OnClickListener cancelKillProgram=new DialogInterface.OnClickListener() {
+    private DialogInterface.OnClickListener cancelKillProgram = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
             dialogInterface.cancel();
@@ -48,30 +48,30 @@ public class MainActivity extends AppCompatActivity {
     };
 
     //玩家在登录界面点击返回按钮
-    public void btnLoginReturnClicked(View view){
+    public void btnLoginReturnClicked(View view) {
         setContentView(R.layout.init_view);
     }
 
     //玩家在登录界面点击登录按钮
-    public void btnLoginClicked(View view){
+    public void btnLoginClicked(View view) {
         //得到玩家账号和密码
-        EditText editText=findViewById(R.id.txtAccount);
-        String accountName=editText.getText().toString();
-        editText=findViewById(R.id.txtPasswd);
-        String password=editText.getText().toString();
+        EditText editText = findViewById(R.id.txtAccount);
+        String accountName = editText.getText().toString();
+        editText = findViewById(R.id.txtPasswd);
+        String password = editText.getText().toString();
 
         //判断用户名是否为空
-        if (accountName==null || accountName.length()==0){
-            AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(this);
+        if (accountName == null || accountName.length() == 0) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setMessage("用户名不能为空！");
             alertDialogBuilder.setPositiveButton("确定", dealWithEmpty);
-            AlertDialog alertDialog=alertDialogBuilder.create();
+            AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show();
-        }else if (password==null || password.length()==0){
-            AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(this);
+        } else if (password == null || password.length() == 0) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
             alertDialogBuilder.setMessage("密码不能为空！");
             alertDialogBuilder.setPositiveButton("确定", dealWithEmpty);
-            AlertDialog alertDialog=alertDialogBuilder.create();
+            AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show();
         }
 
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //玩家输入用户名或密码为空时，弹出对话框的按钮动作
-    private DialogInterface.OnClickListener dealWithEmpty =new DialogInterface.OnClickListener() {
+    private DialogInterface.OnClickListener dealWithEmpty = new DialogInterface.OnClickListener() {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
             dialogInterface.cancel();
